7ae845b1adeb35af108522b5ca489e53138818d9
Adriel da Silva <silvaadriel510@gmail.com>
2019-08-22 15:31:21 -0300

    empty users array message implemented

--- src/pages/Dev.js
+++ src/pages/Dev.js
@@ -50,25 +50,30 @@ const Dev = ({ match }) => {
       <Link to="/">
         <img src={logo} alt="Tindev" />
       </Link>
-      <ul>
-        {users.map(user => (
-          <li key={user._id}>
-            <img src={user.avatar} alt={user.name} />
-            <footer>
-              <strong>{user.name}</strong>
-              <p>{user.bio}</p>
-            </footer>
-            <div className="buttons">
-              <button type="button" onClick={() => handleDislike(user._id)}>
-                <img src={dislike} alt="Dislike" />
-              </button>
-              <button type="button" onClick={() => handleLike(user._id)}>
-                <img src={like} alt="Like" />
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+      { users.length ? (
+          <ul>
+            {users.map(user => (
+              <li key={user._id}>
+                <img src={user.avatar} alt={user.name} />
+                <footer>
+                  <strong>{user.name}</strong>
+                  <p>{user.bio}</p>
+                </footer>
+                <div className="buttons">
+                  <button type="button" onClick={() => handleDislike(user._id)}>
+                    <img src={dislike} alt="Dislike" />
+                  </button>
+                  <button type="button" onClick={() => handleLike(user._id)}>
+                    <img src={like} alt="Like" />
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div className="empty">Come back later.</div>
+        ) 
+      }
     </div>
   );
 };
