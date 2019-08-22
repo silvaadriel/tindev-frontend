5a6a3ec004125ce1b63bfc613ca54fcc8a81edc8
Adriel da Silva <silvaadriel510@gmail.com>
2019-08-22 14:56:32 -0300

    dev route added

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 
 import Login from './pages/Login';
+import Dev from './pages/Dev';
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Route exact path="/" component={Login} />
+      <Route path="/dev/:id" component={Dev} />
     </BrowserRouter>
   );
 };
