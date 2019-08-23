92d9d036dc5e978e87e26bf04d9c2321dbdc41e7
Adriel da Silva <silvaadriel510@gmail.com>
2019-08-23 15:28:19 -0300

    loading implemented to load users

--- src/pages/Dev.js
+++ src/pages/Dev.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../services/api';
 
+import Loading from '../components/Loading';
+
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
@@ -10,6 +12,7 @@ import './Dev.css';
 
 const Dev = ({ match }) => {
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const loadUsers = async () => {
@@ -20,6 +23,7 @@ const Dev = ({ match }) => {
       });
 
       setUsers(response.data);
+      setIsLoading(false);
     };
 
     loadUsers();
@@ -50,29 +54,31 @@ const Dev = ({ match }) => {
       <Link to="/">
         <img src={logo} alt="Tindev" />
       </Link>
-      { users.length ? (
-          <ul>
-            {users.map(user => (
-              <li key={user._id}>
-                <img src={user.avatar} alt={user.name} />
-                <footer>
-                  <strong>{user.name}</strong>
-                  <p>{user.bio}</p>
-                </footer>
-                <div className="buttons">
-                  <button type="button" onClick={() => handleDislike(user._id)}>
-                    <img src={dislike} alt="Dislike" />
-                  </button>
-                  <button type="button" onClick={() => handleLike(user._id)}>
-                    <img src={like} alt="Like" />
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <div className="empty">Come back later.</div>
-        ) 
+      { isLoading
+          ? <div className="loading"><Loading /></div>
+          : users.length ? (
+            <ul>
+              {users.map(user => (
+                <li key={user._id}>
+                  <img src={user.avatar} alt={user.name} />
+                  <footer>
+                    <strong>{user.name}</strong>
+                    <p>{user.bio}</p>
+                  </footer>
+                  <div className="buttons">
+                    <button type="button" onClick={() => handleDislike(user._id)}>
+                      <img src={dislike} alt="Dislike" />
+                    </button>
+                    <button type="button" onClick={() => handleLike(user._id)}>
+                      <img src={like} alt="Like" />
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div className="empty">Come back later.</div>
+          ) 
       }
     </div>
   );
